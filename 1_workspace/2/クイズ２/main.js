4e4379296a73c319f17597d4449ed3ba04b5efa4
ShintaroKatayama <sin.siitaro@gmail.com>
2021-04-21 20:11:55 +0900

    [DogAPIで非同期処理]を学習しました。

---
+++ 1_workspace/2/クイズ２/main.js
@@ -0,0 +1,79 @@
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+const container = document.getElementById('choices-container')
+
+// クイズの内容
+const quiz = {
+  text: "この星の名前は何でしょう？",
+  image: "Ganymede.jpg",
+  choices: [
+    {
+      text: "ゴリアテ",
+      feedback:
+        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+    },
+    {
+      text: "ゼニガメ",
+      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+    },
+    {
+      text: "ガニメデ",
+      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+    },
+  ],
+}
+
+const btn1 = document.createElement('div')
+btn1.classList.add('btn')
+btn1.textContent = quiz.choices[0].text
+container.append(btn1)
+
+const btn2 = document.createElement('div')
+btn2.classList.add('btn')
+btn2.textContent = quiz.choices[1].text
+container.append(btn2)
+
+const btn3 = document.createElement('div')
+btn3.classList.add('btn')
+btn3.textContent = quiz.choices[2].text
+container.append(btn3)
+
+// quiz を画面に表示する関数
+const reloadQuiz = function() {
+  // 問題文を表示
+  quizText.textContent = "Q. " + quiz.text
+
+  // 画像を表示
+  quizImage.src = "./images/" + quiz.image
+
+  // 選択肢（ボタン）の中身を表示
+  choice1.textContent = quiz.choices[0].text
+  choice2.textContent = quiz.choices[1].text
+  choice3.textContent = quiz.choices[2].text
+}
+
+// choiceNumber番目の選択肢を選択
+const choose = function(choiceNumber) {
+  // フィードバックを表示
+  feedback.textContent = quiz.choices[choiceNumber].feedback
+}
+
+choice1.onclick = function() {
+  // 0 番目の選択肢を選択
+  choose(0)
+}
+choice2.onclick = function() {
+  // 1 番目の選択肢を選択
+  choose(1)
+}
+choice3.onclick = function() {
+  // 2 番目の選択肢を選択
+  choose(2)
+}
+
+// reloadQuiz関数 を実行して、クイズを画面に表示する
+reloadQuiz()
